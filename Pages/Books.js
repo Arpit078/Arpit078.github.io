788e4be5823efc0e3915e1c303a88e0f9ee70e86
Arpit078 <verma.arpit078@.com>
2023-07-18 17:09:29 +0530

    finally complete for now

--- Pages/Books.js
+++ Pages/Books.js
@@ -7,11 +7,45 @@ const Books = /*html*/`
             <div>${Navbar}</div>
         </div>
         <div class="content">
-                <p class="subHead">Anime Quote</p>
-                <p class="paragraph">
-                </p>
-               
-        </div>
+        <p class="subHead">Psycho-Cybernetics </p>
+        <p class="paragraph">Reading currently and looks good so far.
+        </p>
+        <p class="highlight">
+        Cover : <br>
+        <img src="https://m.media-amazon.com/images/I/41uiX93jl8L._SX309_BO1,204,203,200_.jpg" class="link paragraph"/>
+        </p>
+        <p class="highlight">
+        Read Date : 
+        <span class="paragraph">July 2023</span>
+        </p>
+    </div>
+        <div class="content">
+        <p class="subHead">Let's Talk Money </p>
+        <p class="paragraph">Was suggested by some youtube video and boy was this book a good. It is so much 
+            informative I'd have to come back to this again and again. Must read if you want to learn about personal finance.
+        </p>
+        <p class="highlight">
+        Cover : <br>
+        <img src="https://m.media-amazon.com/images/I/51YJ9xQuWfS._SX324_BO1,204,203,200_.jpg" class="link paragraph"/>
+        </p>
+        <p class="highlight">
+        Read Date : 
+        <span class="paragraph">June 2023</span>
+        </p>
+    </div>
+        <div class="content">
+        <p class="subHead">Doglapan </p>
+        <p class="paragraph">Stumbled on this as an accident but it was a nice journey. Had me understand the behind the scenes of indian startups.
+        </p>
+        <p class="highlight">
+        Cover : <br>
+        <img src="https://rukminim2.flixcart.com/image/416/416/xif0q/book/l/o/t/doglapan-original-imagmxk5qzfebqwj.jpeg?q=70" class="link paragraph"/>
+        </p>
+        <p class="highlight">
+        Read Date : 
+        <span class="paragraph">June 2023</span>
+        </p>
+    </div>
         
 
      </div>
