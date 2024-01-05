5bc7f0ef0c9987b7b64bc45102d4e1edd42d9b03
Arpit078 <verma.arpit078@.com>
2024-01-06 00:57:18 +0530

    This commit is from batch file

--- Logic/Blogs.js
+++ Logic/Blogs.js
@@ -9,13 +9,40 @@ async function fetchProjects(){
 
 
     for(let i =0;i<blogDatabase.length;i++){
+        let content =``
+        let number = 1
+        for(let j=0;j<blogDatabase[i].blocks.length;j++){
+            let el = blogDatabase[i].blocks[j]
+            switch (el.component) {
+                case "paragraph":
+                    content = content + el.text + '<br><br>'
+                    break;
+                case "bulleted_list_item":
+                    content = content +'&#149; ' + el.text + '<br>'
+                    break;
+                case "numbered_list_item":
+                    content = content + '' + `${number}. ` + el.text + '<br>'
+                    number +=1
+                    break;
+                case "code":
+                    code_text = el.text .replace(/\\/g, '""') // Replace \ with ""
+                                        .replace(/\n/g, '<br>') // Replace \n with <br>
+                                        .replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;'); // Replace \t with HTML tab space
+                    content = content+ '<div class="code_block">' + code_text +'</div>'+ '<br>'
+                    break;
+            
+                default:
+                    break;
+            }
+        }
         const projectObj = `
             <div class="content">
                 <p class="subHead">${blogDatabase[i].title}</p>
-                <p class="paragraph">${blogDatabase[i].content}</p>
+                <p class="paragraph">${content}</p>
                 <p class="highlight">
                 Dated :
-                <span class="paragraph">${blogDatabase[i].date}</span>
+                    <span class="paragraph">${blogDatabase[i].date}</span> 
+
                 </p>
             </div>
     `
