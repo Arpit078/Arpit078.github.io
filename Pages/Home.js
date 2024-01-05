a855f96e8fff7a2d4ecb47a5cf12714c86c45543
Arpit078 <verma.arpit078@.com>
2024-01-06 02:25:20 +0530

    This commit is from batch file

--- Pages/Home.js
+++ Pages/Home.js
@@ -9,11 +9,11 @@ const Home = /*html*/ `
     </div>
     <p class="subHead">My name is Arpit Kumar Verma.</p>
     <p class="paragraph">
-        I am a student pursuing bachelor of technology in Electrical Engineering at IIT Ropar. I am a software development ethusiast. I love working on task automation, scripting and sometimes making web applications. I am the creator of the JavaScript framework <a href="https://www.npmjs.com/package/@arpit078/preactjs" class="link">@arpit078/preactJS<span class="bar"></span></a> that is powering this website. PreactJS is a single page application javascript framework. Most of my work is open sourced and publicly available on 
+        I am a student pursuing bachelor of technology in Electrical Engineering at IIT Ropar. I am a software development ethusiast. I love working on task automation, scripting and making backend systems for web applications. I am the creator of the JavaScript library <a href="https://www.npmjs.com/package/@arpit078/preactjs" class="link">@arpit078/preactJS<span class="bar"></span></a> that is powering this website. PreactJS is a single page application javascript library. Most of my work is open sourced and publicly available on 
     <a href="https://github.com/Arpit078" class="link">Github</a> 
     </p>
     <p class="paragraph">
-        You can connect with me on <a href="https://www.linkedin.com/in/arpit-verma-806268227/" class="link"><span class="bar"></span>LinkedIN </a>. If you happen to also use <a href="https://www.instagram.com/arpitverma.here/" class="link"><span class="bar"></span>Instagram </a>, let's have a chat there.
+        You can connect with me on <a href="https://www.linkedin.com/in/arpit-verma-806268227/" class="link"><span class="bar"></span>LinkedIN </a>.If you have something in mind and want to let me know please get in touch with me on my <a href="mailto:verma.arpit078@gmail.com" class="link"><span class="bar"></span>Mail </a>.
     </p>
     <p class="paragraph">
         Outside of goofing around with scripting and automation, I enjoy playing Basketball, Reading books, Watching anime and maybe travelling sometimes.
