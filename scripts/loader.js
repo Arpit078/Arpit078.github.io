39cddfa1d2bebf211db69e6332aab369f4faef22
Arpit078 <verma.arpit078@.com>
2024-03-02 00:09:48 +0530

    This commit is from batch file

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,2 +1,6 @@
 const dom = document.getElementById("virtual-dom");
-dom.innerHTML = Home;import Blogs from "../Pages/Blogs.js";window.sessionStorage.setItem("Blogs",Blogs);import Books from "../Pages/Books.js";window.sessionStorage.setItem("Books",Books);import Home from "../Pages/Home.js";window.sessionStorage.setItem("Home",Home);import Projects from "../Pages/Projects.js";window.sessionStorage.setItem("Projects",Projects);
+dom.innerHTML = Home;import Blogs from "../Pages/Blogs.js";
+      window.sessionStorage.setItem("Blogs",Blogs);import Books from "../Pages/Books.js";
+      window.sessionStorage.setItem("Books",Books);import Home from "../Pages/Home.js";
+      window.sessionStorage.setItem("Home",Home);import Projects from "../Pages/Projects.js";
+      window.sessionStorage.setItem("Projects",Projects);
--- scripts/router.js
+++ scripts/router.js
@@ -11,36 +11,69 @@ function onNavigate(pathname){
     dom.innerHTML = routes[window.location.hash];
     
 }
+
+function removeScriptBySrc(src) {
+  console.log("in remove script : ")
+  const scripts = document.getElementsByTagName('script');
+  for (let i = 0; i < scripts.length; i++) {
+      // console.log(scripts[i].src)
+      if (scripts[i].src.includes(src)) {
+          scripts[i].parentNode.removeChild(scripts[i]);
+
+      }
+  }
+}
+
 const BlogsData =sessionStorage.getItem("Blogs");
-        function Blogs(){
-            onNavigate("#Blogs");
-            let myScript = document.createElement("script");
-            myScript.setAttribute("src", "../Logic/Blogs.js");
-            document.body.appendChild(myScript);
-        };const BooksData =sessionStorage.getItem("Books");
-        function Books(){
-            onNavigate("#Books");
-            let myScript = document.createElement("script");
-            myScript.setAttribute("src", "../Logic/Books.js");
-            document.body.appendChild(myScript);
-        };const HomeData =sessionStorage.getItem("Home");
-        function Home(){
-            onNavigate("#Home");
-        };const ProjectsData =sessionStorage.getItem("Projects");
-        function Projects(){
-            onNavigate("#Projects");
-            let myScript = document.createElement("script");
-            myScript.setAttribute("src", "../Logic/Projects.js");
-            document.body.appendChild(myScript);
-        };const routes = {"":HomeData,"#Blogs":BlogsData,"#Books":BooksData,"#Home":HomeData,"#Projects":ProjectsData};if(window.location.hash in routes == true){
+function Blogs()
+      {
+          onNavigate("#Blogs");
+          let myScript = document.createElement("script");
+          myScript.setAttribute("src", "../Logic/Blogs.js");
+          removeScriptBySrc("Logic")
+          document.body.appendChild(myScript);
+      };
+      
+      
+const BooksData =sessionStorage.getItem("Books");
+function Books()
+      {
+          onNavigate("#Books");
+          let myScript = document.createElement("script");
+          myScript.setAttribute("src", "../Logic/Books.js");
+          removeScriptBySrc("Logic")
+          document.body.appendChild(myScript);
+      };
+      
+      const HomeData =sessionStorage.getItem("Home");
+            function Home(){
+                onNavigate("#Home");
+            };
+const ProjectsData =sessionStorage.getItem("Projects");
+function Projects()
+      {
+          onNavigate("#Projects");
+          let myScript = document.createElement("script");
+          myScript.setAttribute("src", "../Logic/Projects.js");
+          removeScriptBySrc("Logic")
+          document.body.appendChild(myScript);
+      };
+      
+      
+const routes = {"":HomeData,"#Blogs":BlogsData,"#Books":BooksData,"#Home":HomeData,"#Projects":ProjectsData};
+if(window.location.hash in routes == true)
+    {
         dom.innerHTML = routes[window.location.hash];
     }
-        const logicRoutes =["Blogs","Books","Projects"];
-        if(logicRoutes.includes(window.location.hash.slice(1))){
-            let myScript = document.createElement("script");
-            const filename = window.location.hash.slice(1)
-            const filepath = "../Logic/" + filename + ".js"
-            myScript.setAttribute("src", filepath);
-            document.body.appendChild(myScript);
-        
+
+const logicRoutes =["Blogs","Books","Projects"];
+
+//executes only once for the cases when the user visits the specific route from search
+if(logicRoutes.includes(window.location.hash.slice(1)))
+    {
+        let myScript = document.createElement("script");
+        const filename = window.location.hash.slice(1)
+        const filepath = "../Logic/" + filename + ".js"
+        myScript.setAttribute("src", filepath);
+        document.body.appendChild(myScript);
     }
