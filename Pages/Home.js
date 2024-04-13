5e16de4d409bb934c88841f2fb1d5e2cf36dc25a
Arpit078 <verma.arpit078@.com>
2024-04-14 01:08:20 +0530

    text changes

--- Pages/Home.js
+++ Pages/Home.js
@@ -9,14 +9,14 @@ const Home = /*html*/ `
     </div>
     <p class="subHead">My name is Arpit Kumar Verma.</p>
     <p class="paragraph">
-        I am a student pursuing bachelor of technology in Electrical Engineering at IIT Ropar. I am a software development ethusiast. I love working on task automation, scripting and making backend systems for web applications. I am the creator of the JavaScript library <a href="https://www.npmjs.com/package/@arpit078/preactjs" class="link">@arpit078/preactJS<span class="bar"></span></a> that is powering this website. PreactJS is a single page application javascript library. Most of my work is open sourced and publicly available on 
-    <a href="https://github.com/Arpit078" class="link">Github</a> 
+        I am a student pursuing bachelor of technology in Electrical Engineering at IIT Ropar. I am a software development ethusiast. I love working on task automation, scripting and making backend systems for web applications. I am the creator of the npm package <a href="https://www.npmjs.com/package/@arpit078/preactjs" class="link">@arpit078/preactJS<span class="bar"></span></a> that is powering this website. PreactJS is a SPA(single page applications) development javascript library with 500+ all time downloads. Most of my work is open sourced and publicly available on my
+    <a href="https://github.com/Arpit078" class="link">Github<span class="bar"></span></a> 
     </p>
     <p class="paragraph">
-        You can connect with me on <a href="https://www.linkedin.com/in/arpit-verma-806268227/" class="link"><span class="bar"></span>LinkedIN </a>.If you have something in mind and want to let me know please get in touch with me on my <a href="mailto:verma.arpit078@gmail.com" class="link"><span class="bar"></span>Mail </a>.
+        You can connect with me on <a href="https://www.linkedin.com/in/arpit-verma-806268227/" class="link">LinkedIN <span class="bar"></span></a>.If you have something in mind and want to let me know please get in touch with me on my <a href="mailto:verma.arpit078@gmail.com" class="link"><span class="bar"></span>Mail </a>.
     </p>
     <p class="paragraph">
-        Outside of goofing around with scripting and automation, I enjoy playing Basketball, Reading books, Watching anime and maybe travelling sometimes.
+        Outside of goofing around with backend systems, I enjoy playing Basketball, Reading books, Watching anime and maybe travelling sometimes.
     </p>
     <p class="paragraph">
         And it's definitely not me who took the website design from Evan You's portfolio:}
