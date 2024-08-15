81fa7d86d9f30f59e187a7fbf65f3b491d6378d6
Arpit <92263716+Arpit078@users.noreply.github.com>
2024-08-16 01:31:58 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,6 @@ const Navbar = /*html*/`
     <ul id="navbar">
         <li onclick="Home()">home</li>
         <li onclick="Projects()">projects</li>
-        // <li onclick="Blogs()">Learning in Public</li>
         <li onclick="Books()">Books read</li>
     </ul>
 
