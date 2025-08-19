d5a5390c28a50088c8c45b6ae3f4f3a5f782b316
Arpit078 <verma.arpit078@.com>
2025-08-19 16:09:06 +0530

    update homepage

--- Pages/Home.js
+++ Pages/Home.js
@@ -9,14 +9,14 @@ const Home = /*html*/ `
     </div>
     <p class="subHead">My name is Arpit Kumar Verma.</p>
     <p class="paragraph">
-        I am a final year student at IIT Ropar pursuing bachelor of technology in Electrical Engineering. I Interned at BlueStacks in 2024. I love working on task automation, scripting and making backend systems for web applications. I am the creator of the Javascript framework <a href="https://www.npmjs.com/package/pre-reactjs" class="link">pre-reactJS<span class="bar"></span></a> that is powering this website. Pre-reactJS is a SPA(single page applications) development javascript framework with 500+ all time downloads. Most of my work is open sourced and publicly available on my
+        I am a final year student at IIT Ropar pursuing bachelor of technology in Electrical Engineering. I Interned at BlueStacks in 2024. I love building products that solve real problems and designing scalable backend systems. I built a habit tracker app loved by men, try it - <a href="https://play.google.com/store/apps/details?id=com.microdev.tainted" class="link">Tainted<span class="bar"></span></a>. A lesser known SPA(single page applications) Javascript framework that I created <a href="https://pre-reactjs-docs.vercel.app/" class="link">pre-reactJS<span class="bar"></span></a> is powering this website. Most of my work is open sourced and publicly available on my
     <a href="https://github.com/Arpit078" class="link">Github<span class="bar"></span></a> 
     </p>
     <p class="paragraph">
         You can connect with me on <a href="https://www.linkedin.com/in/arpit-verma-806268227/" class="link">LinkedIN <span class="bar"></span></a>.If you have something in mind and want to let me know please get in touch with me on my <a href="mailto:verma.arpit078@gmail.com" class="link"><span class="bar"></span>Mail </a>.
     </p>
     <p class="paragraph">
-        Outside of goofing around with backend development, I enjoy playing Basketball, Reading books, Watching anime and maybe travelling sometimes.
+        Outside of goofing around with backend development, I enjoy playing Basketball, Reading books and maybe travelling sometimes.
     </p>
     <p class="paragraph">
         And it's definitely not me who took the website design from Evan You's portfolio:}
