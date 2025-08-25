9eee8a7a5fda782d409672c5ba781dd2e8a34700
Arpit078 <verma.arpit078@.com>
2025-08-25 16:08:39 +0530

    readmore css

--- Pages/Blogs.js
+++ Pages/Blogs.js
@@ -1,6 +1,24 @@
 import Navbar from "../components/Navbar.js"
 import Box from "../components/updateBox/box.js"
 const Blogs = /*html*/`
+<style>
+    .paragraph {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 3; /* show only 3 lines */
+  -webkit-box-orient: vertical;
+}
+
+.paragraph.expanded {
+  -webkit-line-clamp: unset; /* remove restriction */
+}
+.read-more-btn {
+  color: rgb(0, 0, 0);
+  cursor: pointer;
+  font-size: 20px;
+}
+
+</style>
 <div id="blogs" class="text">
  <div id="headAndNav">
      <h1 class="title">Blogs.</h1>
