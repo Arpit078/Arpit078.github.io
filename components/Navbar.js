c2437bba54460d91e31f9c55dce7ed63a668b181
Arpit078 <verma.arpit078@.com>
2025-10-01 03:51:21 +0530

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,8 @@
 const Navbar = /*html*/`
     <ul id="navbar">
-        <li onclick="Home()">home</li>
-        <li onclick="Projects()">projects</li>
-        <li onclick="Blogs()">blogs</li>
+        <li onclick="Home()">Home</li>
+        <li onclick="Projects()">Projects</li>
+        <li onclick="Blogs()">Blogs</li>
         <li onclick="Books()">Books read</li>
     </ul>
 
