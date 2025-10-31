015ee5092d594f93bb51d2f3993e7b72e58273d2
Arpit078 <verma.arpit078@.com>
2025-10-31 22:29:32 +0530

    This commit is from batch file

--- Pages/Home.js
+++ Pages/Home.js
@@ -9,7 +9,7 @@ const Home = /*html*/ `
     </div>
     <p class="subHead">My name is Arpit Kumar Verma.</p>
     <p class="paragraph">
-        I’m a graduate of IIT Ropar with a B.Tech in Electrical Engineering. I Interned at BlueStacks in 2024. I love building products that solve real problems and designing scalable backend systems. I built a habit tracker app loved by men, try it - <a href="https://play.google.com/store/apps/details?id=com.microdev.tainted" class="link">Tainted<span class="bar"></span></a>. A lesser known SPA(single page applications) Javascript framework that I created <a href="https://pre-reactjs-docs.vercel.app/" class="link">pre-reactJS<span class="bar"></span></a> is powering this website. Most of my work is open sourced and publicly available on my
+        I am a Software Developer at <a href="https://jiohotstar.com" class="link">JioHotstar<span class="bar"></span></a> . I graduated from IIT Ropar with a B.Tech in Electrical Engineering in 2025. I love building products that solve real problems and designing scalable backend systems. I built a habit tracker app loved by men, try it - <a href="https://play.google.com/store/apps/details?id=com.microdev.tainted" class="link">Tainted<span class="bar"></span></a>. A lesser known SPA(single page applications) Javascript framework that I created <a href="https://pre-reactjs-docs.vercel.app/" class="link">pre-reactJS<span class="bar"></span></a> is powering this website. Most of my work is open sourced and publicly available on my
     <a href="https://github.com/Arpit078" class="link">Github<span class="bar"></span></a> 
     </p>
     <p class="paragraph">
