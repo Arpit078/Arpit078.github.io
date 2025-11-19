9e59a05786c7db339c2a9e3998e3a7ae9dd57c04
Arpit078 <verma.arpit078@.com>
2025-11-19 10:04:23 +0530

    homepage copy

--- Pages/Home.js
+++ Pages/Home.js
@@ -4,36 +4,32 @@ import Navbar from "../components/Navbar.js"
 const Home = /*html*/ `
 <div class="text home-content">
     <div id="headAndNav">
+        <h1 class="title">Hi.</h1>
         <div class="bg">${Navbar}</div>
     </div>
-    <p class="subHead">Hi, I'm Arpit Kumar Verma.</p>
+    <p class="subHead">I'm Arpit Kumar Verma.</p>
 
 <p class="paragraph">
-    I’m a Software Engineer at <a href="https://jiohotstar.com" class="link">JioHotstar<span class="bar"></span></a>. 
-    I graduated from IIT Ropar in 2025 with a B.Tech in Electrical Engineering. 
-    I love building products that solve real problems and designing scalable backend systems. 
-    I built a habit tracker, loved by men, try it - <a href="https://play.google.com/store/apps/details?id=com.microdev.tainted" class="link">Tainted<span class="bar"></span></a>.
-    I also created a lesser-known JavaScript SPA (Single Page Application) framework called 
-    <a href="https://pre-reactjs-docs.vercel.app/" class="link">pre-reactJS<span class="bar"></span></a>, 
-    which actually powers this website.  
-    Most of my work is open-source and available on 
-    <a href="https://github.com/Arpit078" class="link">GitHub<span class="bar"></span></a>.
+   I’m a Software Development Engineer at <a href="https://jiohotstar.com" class="link">JioHotstar<span class="bar"></span></a>. I graduated from IIT Ropar in 2025 with a B.Tech in Electrical Engineering.
 </p>
 
 <p class="paragraph">
-    You can connect with me on 
-    <a href="https://www.linkedin.com/in/arpit-verma-806268227/" class="link">LinkedIn<span class="bar"></span></a>, 
-    or reach out directly via 
-    <a href="mailto:verma.arpit078@gmail.com" class="link">email<span class="bar"></span></a> 
-    if you’d like to collaborate or just say hi.
+    I love building products that solve real problems.
+    I built a community driven habit tracker app, check it out: <a href="https://play.google.com/store/apps/details?id=com.microdev.tainted" class="link">Tainted<span class="bar"></span></a>.
+    I also created a lesser-known SPA (Single Page Application) framework called 
+    <a href="https://pre-reactjs-docs.vercel.app/" class="link">pre-reactJS<span class="bar"></span></a>, 
+    which actually powers this website.  
+</p>
+
+<p class="paragraph">
+    Most of my work is open-source and publicly available on 
+    <a href="https://github.com/Arpit078" class="link">GitHub<span class="bar"></span></a>.
+    You can also find me on 
+    <a href="https://www.linkedin.com/in/arpit-verma-806268227/" class="link">LinkedIn.<span class="bar"></span></a>
 </p>
 
 <p class="paragraph">
     Outside work, I enjoy playing basketball, reading books, and traveling sometimes.
-</p>
-
-<p class="paragraph">
-    And no, I definitely didn’t borrow this website design from Evan You’s portfolio :}
 </p>
 
 
